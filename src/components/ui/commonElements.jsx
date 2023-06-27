d71a328f37991a8569636fa8ed401cbda5ea030b
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-06-27 18:40:47 -0300

    :recycle: refactor: Componentizando o Input em commonElements.jsx

--- src/components/ui/commonElements.jsx
+++ src/components/ui/commonElements.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { colorBranco, colorRoxo } from './variaveis';
+import { colorPreto, colorVerde } from 'components/ui/variaveis';
 
 export const SectionTitle = styled.h3`
     font-size: 1.75rem;
@@ -11,6 +12,7 @@ export const Botao = styled.button`
     background-color: ${colorRoxo};
 
     width: 6.375rem;
+    height: 2.375rem;
 
     border: none;
 
@@ -21,4 +23,26 @@ export const Botao = styled.button`
     letter-spacing: 0.02856rem;
 
     color: ${colorBranco};
+`
+
+export const Input = styled.input`
+    height: 3em;
+    width: 320px;
+    padding: 1em;
+    font-size: 12px;
+
+    border: 1px solid ${colorPreto};
+
+    &:focus {
+        outline: none;
+        border: 2px solid ${colorVerde};
+    }
+
+    @media (min-width: 768px) {
+        width: 9.0625rem;
+    }
+
+    @media (min-width: 1440px) {
+        width: 320px ;
+    }
 `
