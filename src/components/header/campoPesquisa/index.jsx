3b2e21ae63775ffe3353a7af5e14b3e92e8447e2
Fabiano Raiser <fabiano.raiser16@gmail.com>
2023-06-27 18:43:34 -0300

    :recycle: refactor: refatorando o input do campo de pesquisa

--- src/components/header/campoPesquisa/index.jsx
+++ src/components/header/campoPesquisa/index.jsx
@@ -1,30 +1,7 @@
-import { colorPreto, colorVerde } from "components/ui/variaveis"
-import styled from "styled-components"
-
-const StyledCampoPesquisa = styled.input`
-    height: 3em;
-    width: 320px;
-    padding: 1em;
-    font-size: 12px;
-
-    border: 1px solid ${colorPreto};
-
-    &:focus {
-        outline: none;
-        border: 2px solid ${colorVerde};
-    }
-
-    @media (min-width: 768px) {
-        width: 9.0625rem;
-    }
-
-    @media (min-width: 1440px) {
-        width: 320px ;
-    }
-`
+import { Input } from "components/ui/commonElements"
 
 export const CampoPesquisa = () => {
     return (
-        <StyledCampoPesquisa type='text' placeholder="Digite o produto"/>
+        <Input type='text' placeholder="Digite o produto"/>
     )
 }
